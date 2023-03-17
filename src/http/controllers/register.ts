4a0563490ec8bb6ebabdd07d3bef6ffe22097a0e
vitordiniz25 <vitordiniz25@gmail.com>
2023-03-16 22:47:20 -0300

    chore: using dependency inversion principle

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
-import { prisma } from "@/lib/prisma"
-import { hash } from "bcryptjs"
 import { z } from "zod"
 import { FastifyRequest, FastifyReply } from "fastify"
-import { registerUseCase } from "@/use-cases/register"
+import { RegisterUseCase } from "@/use-cases/register"
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository"
 
 export async function register(request: FastifyRequest, reply: FastifyReply)  {
     const registerBodySchema = z.object({
@@ -14,7 +13,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply)  {
     const { name, email, password } = registerBodySchema.parse(request.body)
 
     try {
-        await registerUseCase({
+        const usersRepository = new PrismaUsersRepository()
+        const registerUseCase = new RegisterUseCase(usersRepository)
+
+        await registerUseCase.execute({
             name,
             email,
             password
