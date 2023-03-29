af77bb66a40070604299077149a89adb3d4e4987
vitordiniz25 <vitordiniz25@gmail.com>
2023-03-29 15:27:11 -0300

    fix: id for check in in check-in repository

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -7,7 +7,7 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
   async create(data: Prisma.CheckinUncheckedCreateInput) {
     const checkIn = {
-      id: randomUUID,
+      id: randomUUID(),
       user_id: data.user_id,
       gym_id: data.gym_id,
       validated_at: data.validated_at ? new Date(data.validated_at) : null,
