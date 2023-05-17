f1a56bedda6f8ac93c0699999ea0c9ff80d5a996
vitordiniz25 <vitordiniz25@gmail.com>
2023-05-17 15:08:01 -0300

    add: repositorios prisma da academia

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,46 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+        SELECT * from gyms
+        WHERE ( 6371 * acos( cos( radians(${params.latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${params.longitude}) ) + sin( radians(${params.longitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const newGym = await prisma.gym.create({
+      data,
+    })
+
+    return newGym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+}
