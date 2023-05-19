92ec9c392e682dccf0ad99aac97b8d8df422cdf7
vitordiniz25 <vitordiniz25@gmail.com>
2023-05-19 13:39:02 -0300

    add: princípios do JWT e rota de profile

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // Authenticated routes
+
+  app.get('/me', profile)
 }
