7ca3900fd1077b596a20394ca10645f664e9f29e
vitordiniz25 <vitordiniz25@gmail.com>
2023-05-24 19:54:03 -0300

    add: search gyms controller

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 import { FastifyInstance } from 'fastify'
+import { search } from './search'
+import { nearby } from './nearby'
+import { create } from './create'
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
+
+  app.get('/gyms/search', search)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
