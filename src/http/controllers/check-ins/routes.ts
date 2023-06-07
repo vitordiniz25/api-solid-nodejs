8ecfe233082ca58751eeff1b7979304f6c02f34c
vitordiniz25 <vitordiniz25@gmail.com>
2023-06-07 12:19:08 -0300

    add: role based authorization implemented

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -4,6 +4,7 @@ import { history } from './history'
 import { metrics } from './metrics'
 import { create } from './create'
 import { validate } from './validate'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function checkInsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJwt)
@@ -13,5 +14,9 @@ export async function checkInsRoutes(app: FastifyInstance) {
 
   app.post('/gyms/:gymId/check-ins', create)
 
-  app.patch('/check-ins/:checkInId/validate', validate)
+  app.patch(
+    '/check-ins/:checkInId/validate',
+    { onRequest: [verifyUserRole('ADMIN')] },
+    validate,
+  )
 }
--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -23,7 +23,7 @@ export async function authenticate(
     })
 
     const token = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
@@ -32,7 +32,7 @@ export async function authenticate(
     )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      { role: user.role },
       {
         sign: {
           sub: user.id,
