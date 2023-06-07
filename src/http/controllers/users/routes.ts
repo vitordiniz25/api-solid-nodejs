b0801d189bf5a3db9e987836c65693f6de076599
vitordiniz25 <vitordiniz25@gmail.com>
2023-06-07 11:31:14 -0300

    add: refresh token implementacao

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from './authenticate'
 import { register } from './register'
 import { profile } from './profile'
 import { verifyJwt } from '../../middlewares/verify-jwt'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   // Authenticated routes
 
